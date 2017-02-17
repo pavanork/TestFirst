b17b9ba19c3560a4ba57fcfb893b75e869c1b382
pavan <pavanork@gmail.com>
2017-02-17 15:26:54 +0530

    Updated few scripts #1

--- Trails/Program.cs
+++ Trails/Program.cs
@@ -11,6 +11,7 @@ namespace Trails
         static void Main(string[] args)
         {
             Console.WriteLine("hello Git!!");
+            Console.WriteLine("You are awesomely awesome Git!!");
             Console.ReadLine();
 
         }
