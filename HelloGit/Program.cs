7b18dba990e1b3c54cb8dd10361faae3958ce599
Pavan N <pavanork@gmail.com>
2017-02-28 19:42:57 +0530

    updates
    testing the cast

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -10,8 +10,23 @@ namespace HelloGit
     {
         static void Main(string[] args)
         {
+            int number;
+            int Number = 1;
+            const float pi = 3.14f;
+            bool isWorking = false; ;
+            number = 0;//You need to inititalize the variable unless a compile error gets fired
+            Console.WriteLine($"{number} {Number} {pi}{isWorking}");
+            Console.WriteLine();
             Console.WriteLine("hello Git!!");
             Console.WriteLine("You are awesomely awesome Git!!");
+
+            int xx=300;
+            byte bb = (byte)xx;
+            Console.WriteLine(bb);
+            //Console.WriteLine($"{ Convert.ToInt32("A")}");
+
+            double x = 9.89;
+            int y = (int)x;
             Console.ReadLine();
 
         }
